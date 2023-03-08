6395eb817b9c5042dda153c5cd246f91454f6c1e
Xuwenjia <49391729+JenniferLiXu@users.noreply.github.com>
2023-03-08 13:33:26 +0100

    MixtureGaussian_as_target_distribution

--- MH_MCMC.R
+++ MH_MCMC.R
@@ -1,69 +1,70 @@
-#Define target distribution(sinus function)
-target <- function(x) {
-  sin(x)
+#Define a mixture of two Gaussian distributions
+f <- function(x, mu, sigma1, sigma2, w) {
+  w * dnorm(x, mean = 2, sd = sigma1) + (1 - w) * dnorm(x, mean = 2 - mu, sd = sigma2)
 }
 
-# Define the proposal distribution(a mixture of two gaussian distribution)
-proposal <- function(x, mu, sigma, w) {
-  if (runif(1) < w) {
-    rnorm(1, mean = x, sd = 1)
-  } else {
-    rnorm(1, mean = x - mu, sd = sigma)
-  }
-}
-
-likeli <- function(x, mu, m, sigma, w) {
-  w*dnorm(x, mean = m, sd = 1) + (1-w)*dnorm(x-mu, mean = m, sd = sigma)
+#proposal distribution function
+proposal <- function(x, sd) {
+  rnorm(1, mean = x, sd = sd)
 }
 
 #initialize
-set.seed(121) 
+set.seed(123)
 n <- 10000
+mu <- 2
+sigma1 <- 1
+sigma2 <- 2
+w <- 0.6
+sd <- 0.5
+
 x <- numeric(n)
 accept <- numeric(n)
-
-mu <- 2
-sigma <- 0.5
-w <- 0.7
+f_value <- numeric(n)
+f_value[1] <- f(0, mu, sigma1, sigma2, w)
 
 #generate samples using the Metropolis-Hastings algorithm
 for (i in 2:n) {
   # Generate candidate sample
-  y <- proposal(x[i-1],  mu, sigma, w)
-  
+  y <- proposal(x[i - 1], sd)
+  f_proposed <- f(y, mu, sigma1, sigma2, w)
   # Calculate acceptance ratio
-    alpha <- min(1, target(y) * likeli(y, mu, x[i-1],sigma, w) /
-                 (target(x[i-1])*likeli(x[i-1], mu, y,sigma, w) ))
+  alpha <- min(1, f_proposed / f_value[i - 1])
 
   # Decide whether to accept or reject the candidate sample
   if (runif(1) < alpha) {
     x[i] <- y
     accept[i] <- 1
+    f_value[i] <- f_proposed
   }else {
-    x[i] <- x[i-1]
+    x[i] <- x[i - 1]
     accept[i] <- 0
+    f_value[i] <- f_value[i - 1]
   }
-  
 }
+
+#target \int \sin(x) p(x) dx
+esti_sin <- mean(sin(x))
+
+#numerical integral
+num_sin <- integrate(function(x) sin(x) * f(x, mu, sigma1, sigma2, w), -Inf, Inf)
+
+#difference between estimated value and numerical integral
+abs(esti_sin - num_sin$value)
 
 # Print the acceptance rate
 cat("Acceptance rate:", mean(accept), "\n")
 
-par(mfrow = c(1,1))
 # Plot the samples and the target distribution
-hist(x, breaks = 100, freq = FALSE, main = "mixture of two normal distributions")
-curve(target(x), add = TRUE, col = 'red', lwd = 2)
-abline(v = mean(x[-(1:burnIn)]))
+hist(x, breaks = 30, freq = FALSE, main = "mixture of two normal distributions")
+curve(f(x, mu, sigma1, sigma2, w), add = TRUE, col = "red", lwd = 2)
 
 library(mixtools)
-fit <- normalmixEM(x, k = 2) #we are fitting a mixture of two Gaussian distributions
+#we are fitting a mixture of two Gaussian distributions
+fit <- normalmixEM(x, k = 2)
 # Plot the fitted mixture model
 curve(fit$lambda[1] * dnorm(x, fit$mu[1], sqrt(fit$sigma[1]^2)) +
         fit$lambda[2] * dnorm(x, fit$mu[2], sqrt(fit$sigma[2]^2)),
       from = min(x), to = max(x), add = TRUE, col = "blue")
 
-
-#plot(x[-(1:burnIn)], type = "l", xlab="" , main = "Chain values of x", )
-
-
-
+burnin <- 2000
+plot(x[-(1:burnin)], type = "l", xlab = "", main = "Chain values of x", )
