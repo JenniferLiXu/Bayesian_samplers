7aa5fb8a678a408255efdee51af1ad5a1e98522e
Xuwenjia <49391729+JenniferLiXu@users.noreply.github.com>
2023-03-31 17:42:11 +0200

    MH-to-function

--- MH_MCMC.R
+++ MH_MCMC.R
@@ -1,69 +1,29 @@
-#Define target distribution(sinus function)
-target <- function(x) {
-  sin(x)
-}
-
-# Define the proposal distribution(a mixture of two gaussian distribution)
-proposal <- function(x, mu, sigma, w) {
-  if (runif(1) < w) {
-    rnorm(1, mean = x, sd = 1)
-  } else {
-    rnorm(1, mean = x - mu, sd = sigma)
-  }
-}
-
-likeli <- function(x, mu, m, sigma, w) {
-  w*dnorm(x, mean = m, sd = 1) + (1-w)*dnorm(x-mu, mean = m, sd = sigma)
-}
-
-#initialize
-set.seed(121) 
-n <- 10000
-x <- numeric(n)
-accept <- numeric(n)
-
-mu <- 2
-sigma <- 0.5
-w <- 0.7
-
-#generate samples using the Metropolis-Hastings algorithm
-for (i in 2:n) {
-  # Generate candidate sample
-  y <- proposal(x[i-1],  mu, sigma, w)
+MH_MCMC <- function(target, proposal, x_init, n_iter) {
+  #target: the target distribution
+  #proposal: the proposal distribution function
+  #x_init: starting value of the chain
+  #n_iter: number of iterations
+  chain <- numeric(n_iter)
+  accept <- numeric(n_iter)
+  chain[1] <- x_init
+  f_value <- target(x_init)
   
-  # Calculate acceptance ratio
-    alpha <- min(1, target(y) * likeli(y, mu, x[i-1],sigma, w) /
-                 (target(x[i-1])*likeli(x[i-1], mu, y,sigma, w) ))
-
-  # Decide whether to accept or reject the candidate sample
-  if (runif(1) < alpha) {
-    x[i] <- y
-    accept[i] <- 1
-  }else {
-    x[i] <- x[i-1]
-    accept[i] <- 0
+  for(i in 2:n_iter){
+    # Generate candidate sample
+    y <- proposal(chain[i - 1])
+    f_proposed <- target(y, returnGrad = FALSE)
+    # Calculate acceptance ratio
+    alpha <- min(1, f_proposed / f_value)
+    
+    # Decide whether to accept or reject the candidate sample
+    if (runif(1) < alpha) {
+      chain[i] <- y
+      accept[i] <- 1
+      f_value <- f_proposed
+    }else {
+      chain[i] <- chain[i - 1]
+      accept[i] <- 0
+    }
   }
-  
+  return(chain)
 }
-
-# Print the acceptance rate
-cat("Acceptance rate:", mean(accept), "\n")
-
-par(mfrow = c(1,1))
-# Plot the samples and the target distribution
-hist(x, breaks = 100, freq = FALSE, main = "mixture of two normal distributions")
-curve(target(x), add = TRUE, col = 'red', lwd = 2)
-abline(v = mean(x[-(1:burnIn)]))
-
-library(mixtools)
-fit <- normalmixEM(x, k = 2) #we are fitting a mixture of two Gaussian distributions
-# Plot the fitted mixture model
-curve(fit$lambda[1] * dnorm(x, fit$mu[1], sqrt(fit$sigma[1]^2)) +
-        fit$lambda[2] * dnorm(x, fit$mu[2], sqrt(fit$sigma[2]^2)),
-      from = min(x), to = max(x), add = TRUE, col = "blue")
-
-
-#plot(x[-(1:burnIn)], type = "l", xlab="" , main = "Chain values of x", )
-
-
-
